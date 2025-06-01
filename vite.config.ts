fe34537b3191b046c64767fa8542c2796b1e09c1
tsim-imai <imai@trafficsim.co.jp>
2025-06-02 00:46:31 +0900

    不要なファイルを削除し、Vite設定を更新。index.htmlとtsconfig.app.jsonを削除し、package.jsonからテスト関連のスクリプトを削除。Viteの設定を修正し、出力ディレクトリをdistに変更。

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron'
 import electronRenderer from 'vite-plugin-electron-renderer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'public',
   plugins: [
-    react(),
     electron([
       {
         // Main process entry point
-        entry: 'electron/main.ts',
+        entry: '../electron/main.ts',
         onstart(options) {
           // Notify the Renderer process to reload the page when the Preload script build is complete
           options.reload()
@@ -18,7 +17,7 @@ export default defineConfig({
       },
       {
         // Preload script
-        entry: 'electron/preload.ts',
+        entry: '../electron/preload.ts',
         onstart(options) {
           options.reload()
         },
@@ -39,6 +38,8 @@ export default defineConfig({
     // Ensure compatibility with Electron
     target: 'esnext',
     minify: process.env.NODE_ENV === 'production',
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       external: ['electron'],
       output: {
